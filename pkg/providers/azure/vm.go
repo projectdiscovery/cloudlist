dd1b0b4629b5868259a6cac8fc8463651d73af32
Sajad Parra <parrasajad@gmail.com>
2021-07-29 10:01:25 +0530

    update azure compute and network services to 2021(latest) version

--- pkg/providers/azure/vm.go
+++ pkg/providers/azure/vm.go
@@ -3,8 +3,8 @@ package azure
 import (
 	"context"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
-	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-11-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2021-03-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-02-01/network"
 
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/resources"
 	"github.com/Azure/go-autorest/autorest"
