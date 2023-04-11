5699fcfa1f4245c0c99f4e845b34fbf40316e55c
Sandeep Singh <sandeep@projectdiscovery.io>
2023-04-11 13:43:28 +0530

    Commit message: Update version of cloudlist and add Discord webhook to .github/workflows/release-binary.yml (#333)

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -6,23 +6,19 @@ import (
 )
 
 const banner = `
-   ________                _____      __ 
-  / ____/ /___  __  ______/ / (_)____/ /_
- / /   / / __ \/ / / / __  / / / ___/ __/
-/ /___/ / /_/ / /_/ / /_/ / / (__  ) /_  
-\____/_/\____/\__,_/\__,_/_/_/____/\__/  v1.0.2							  
+  _______             _____     __ 
+ / ___/ /__  __ _____/ / (_)__ / /_
+/ /__/ / _ \/ // / _  / / (_-</ __/
+\___/_/\___/\_,_/\_,_/_/_/___/\__/ 
 `
 
 // version is the current version of cloudlist
-const version = `1.0.2`
+const version = `1.0.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
 	gologger.Print().Msgf("%s\n", banner)
 	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
-
-	gologger.Print().Msgf("Use with caution. You are responsible for your actions\n")
-	gologger.Print().Msgf("Developers assume no liability and are not responsible for any misuse or damage.\n")
 }
 
 // GetUpdateCallback returns a callback function that updates cloudlist
