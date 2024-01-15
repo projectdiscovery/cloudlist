7b2f1a3f5269ff6953eeb817d6792c66d39e629b
Sandeep Singh <sandeep@projectdiscovery.io>
2024-01-15 14:05:38 +0530

    Clear unique map + cname exclusion (#471)
    * feat: clear unique map
    
    * feat: do not display cname value for cloudflare dns
    
    * misc update
    
    ---------
    
    Co-authored-by: Ice3man <nizamulrana@gmail.com>

--- pkg/providers/cloudflare/dns.go
+++ pkg/providers/cloudflare/dns.go
@@ -38,6 +38,10 @@ func (d *dnsProvider) GetResource(ctx context.Context) (*schema.Resources, error
 				DNSName:  record.Name,
 				ID:       d.id,
 			})
+			// Skip CNAME records values to discard duplidate data
+			if record.Type == "CNAME" {
+				continue
+			}
 			list.Append(&schema.Resource{
 				Public:     true,
 				Provider:   providerName,
