8305b48f2bd992fe526b98965c6844c70461d632
sandeep <8293321+ehsandeep@users.noreply.github.com>
2024-02-21 15:47:11 +0530

    version update

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -13,7 +13,7 @@ const banner = `
 `
 
 // version is the current version of cloudlist
-const version = `1.0.6`
+const version = `1.0.7`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
