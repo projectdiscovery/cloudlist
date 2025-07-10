280296a42e75545a520a7318d79232f66be3b955
Sandeep Singh <sandeep@projectdiscovery.io>
2025-07-11 04:57:59 +0530

    GCP Asset API for Org (#688)
    * Added GCP org asset API
    
    * added docs
    
    * feat: gcp provider misc fixes
    
    * Added TPU / Filestore service
    
    * feat: simplify code + misc
    
    * multi cloud docs update + panic check
    
    ---------
    
    Co-authored-by: Ice3man <nizamulrana@gmail.com>

--- pkg/providers/gcp/gke.go
+++ pkg/providers/gcp/gke.go
@@ -11,15 +11,14 @@ import (
 	container "google.golang.org/api/container/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 )
 
-// gkeProvider is a provider for aws Route53 API
+// gkeProvider is a provider for GCP GKE API
 type gkeProvider struct {
 	id       string
-	svc      *container.Service
+	gke      *container.Service // For original approach
 	projects []string
 }
 
@@ -27,8 +26,13 @@ func (d *gkeProvider) name() string {
 	return "gke"
 }
 
-// GetResource returns all the resources in the store for a provider.
+// GetResource returns all the GKE resources in the store for a provider.
 func (d *gkeProvider) GetResource(ctx context.Context) (*schema.Resources, error) {
+	return d.getResourcesWithGKEService(ctx)
+}
+
+// getResourcesWithGKEService uses the original GKE service approach
+func (d *gkeProvider) getResourcesWithGKEService(ctx context.Context) (*schema.Resources, error) {
 	list := schema.NewResources()
 
 	for _, project := range d.projects {
@@ -76,7 +80,7 @@ func (d *gkeProvider) getK8sClusterConfigs(ctx context.Context, projectId string
 	}
 
 	// Ask Google for a list of all kube clusters in the given project.
-	resp, err := d.svc.Projects.Zones.Clusters.List(projectId, "-").Context(ctx).Do()
+	resp, err := d.gke.Projects.Zones.Clusters.List(projectId, "-").Context(ctx).Do()
 	if err != nil {
 		return nil, fmt.Errorf("clusters list project=%s: %w", projectId, err)
 	}
