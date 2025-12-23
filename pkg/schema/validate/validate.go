70e0959d1175c57e8da334c9344184e70574ad75
srivathsan <hey@srivathsan.online>
2025-12-23 16:59:08 +0530

    feat: add sanitization for private IPs and implement related tests, added per project calls instead of fetching entire org to reduce overhead

--- pkg/schema/validate/validate.go
+++ pkg/schema/validate/validate.go
@@ -10,10 +10,11 @@ import (
 var ipv4PrivateRanges = []string{
 	"0.0.0.0/8",       // Current network (only valid as source address)
 	"10.0.0.0/8",      // Private network
-	"100.64.0.0/10",   // Shared Address Space
+	"100.64.0.0/10",   // Shared Address Space (CGNAT)
 	"127.0.0.0/8",     // Loopback
 	"169.254.0.0/16",  // Link-local (Also many cloud providers Metadata endpoint)
-	"172.16.0.0/12",   // Private network
+	"172.16.0.0/12",   // Private network (RFC 1918)
+	"172.64.0.0/10",   // Extended private range (cloud VPCs often use 172.x.x.x)
 	"192.0.0.0/24",    // IETF Protocol Assignments
 	"192.0.2.0/24",    // TEST-NET-1, documentation and examples
 	"192.88.99.0/24",  // IPv6 to IPv4 relay (includes 2002::/16)
