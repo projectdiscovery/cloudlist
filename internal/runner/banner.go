ff1a9524d6d80d474e4175c873c8efc1089d3296
Doğan Can Bakır <dogancanbakir@protonmail.com>
2026-01-21 15:37:23 +0300

    bump version

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -13,7 +13,7 @@ const banner = `
 `
 
 // version is the current version of cloudlist
-const version = `1.2.3`
+const version = `1.3.0`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
